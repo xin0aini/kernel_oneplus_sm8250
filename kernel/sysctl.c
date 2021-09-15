79ccd9b4936dcea780eb8e5cb596b2299c9031a7
YumeMichi <do4suki@gmail.com>
2021-09-16 02:45:14 +0800

    Revert "mm: Hard-code maximum swappiness to 200"
    This reverts commit fd39447393f8d7d9ff6593d4eee6f9e7cc69eff7.
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: Ia55a23def411bf14dc86ebe252094923bc0b2950

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -136,7 +136,6 @@ static unsigned long one_ul = 1;
 static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
 static int one_thousand = 1000;
-static int max_swappiness = 200;
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
 #endif
@@ -1754,7 +1753,7 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
-		.extra2		= &max_swappiness,
+		.extra2		= &one_hundred,
 	},
 	{
 		.procname       = "want_old_faultaround_pte",
